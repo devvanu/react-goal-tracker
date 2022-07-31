c00c68504912afb92650a7f0fcc53e2e679959cc
Anu Kumari <anukumari21529@gmail.com>
2022-07-31 12:08:35 +0530

    updated the files acc to components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,46 @@
-import './App.css'
+import { useState } from 'react';
+
+import GoalInput from './components/Goals/GoalInput/GoalInput';
+import GoalList from './components/Goals/GoalList/GoalList';
+import './App.css';
+
+const DUMMY_GOALS = [
+  { id: 'g1', text: 'Added Goal will be listed here' },
+  { id: 'g2', text: 'You can delete the Goal by clicking it' },
+];
 
 const App = () => {
+  const [goals, setGoals] = useState(DUMMY_GOALS);
+
+  const addGoalHandler = (goalTxt)=> {
+    if(goalTxt !== ''){
+      const goal = {
+        id: Date.now() + '' + Math.floor(Math.random()*78),
+        text: goalTxt
+      }
+      setGoals( prevGoals=> {
+        return [goal, ...prevGoals]
+      })
+    }
+  }
+
+  const deleteGoalHandler = (goalId)=>{
+    setGoals(prevGoals=>{
+      const updatedGoals = prevGoals.filter(goal => goal.id !== goalId);
+      return updatedGoals;
+    })
+  }
+
   return (
-    <div>App</div>
-  );
+    <div>
+      <section id="goal-form">
+        <GoalInput addGoalText={addGoalHandler} />
+      </section>
+      <section id="goals">
+        <GoalList goalItems={goals} deleteGoal={deleteGoalHandler} />
+      </section>
+    </div>
+  )
 }
 
-export default App
+export default App;
